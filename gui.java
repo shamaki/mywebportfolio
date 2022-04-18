b802c9634cffa392ffdcbbfec421bb0db97b63a4
Jeremiah <shamakijeremiah@gmail.com>
2022-04-18 09:58:31 +0900

    Add Gui.java in gui branch

---
+++ gui.java
@@ -0,0 +1,20 @@
+// Gui . java
+
+class Person{
+	String name;
+	int age;
+	
+	Person(String name, int age){
+		this.name = name;
+		this.age = age;
+	}
+
+}
+
+public void static main(String arg) {
+	Person p = new Person("Jerry", 22);
+	printf(p);
+	print("Modification in the Github website");
+	System.out.println("Hello world mY jAVA");
+	System.out.println("Hello world mY jAVA");
+}
